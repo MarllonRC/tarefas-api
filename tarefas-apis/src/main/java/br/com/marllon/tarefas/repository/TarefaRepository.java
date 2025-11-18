0a91ebb7e835cead572438f3947b2a1ea23bf438
Marllon Rafael <121911201+MarllonRC@users.noreply.github.com>
2025-11-18 15:02:53 -0300

    Add files via upload

---
+++ tarefas-apis/src/main/java/br/com/marllon/tarefas/repository/TarefaRepository.java
@@ -0,0 +1,9 @@
+package br.com.marllon.tarefas.repository;
+
+import br.com.marllon.tarefas.model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+}
